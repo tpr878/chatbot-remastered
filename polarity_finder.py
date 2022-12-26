d6df34c3e78625244ce4fd6d5a6f2d4e4727bd6d
tpr878 <67946431+tpr878@users.noreply.github.com>
2022-12-26 13:15:45 +0530

    Add files via upload

---
+++ polarity_finder.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+con = sqlite3.connect('chatbot.db')
+cursorObj = con.cursor()
+
+def polarity_finder(w):
+
+    sql = " SELECT * FROM chatbot WHERE word = ?"
+    word = (w,)
+    cursorObj.execute(sql,word)
+
+    myresult = cursorObj.fetchone()
+
+    print(myresult)
+
+polarity_finder("भूल")
